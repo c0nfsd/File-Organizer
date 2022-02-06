6db5d1cb775681d19e298ed85ca4420a4ab9b1e4
c0nfsd <pathakmilindd@gmail.com>
2022-02-06 16:46:46 +0530

    implemented get extension, make folders and move files functions

--- organizer.py
+++ organizer.py
@@ -3,6 +3,30 @@ import os
 
 path = ""
 files = []
+ext = set()
+
+
+def get_ext():
+    for i in files:
+        if i.is_file():
+            ext.add(i.suffix.replace(".", ""))
+
+
+def make_folder():
+    for i in ext:
+        try:
+            # print()
+            os.mkdir(path / i)
+        except:
+            pass
+
+
+def move_files():
+    for i in files:
+        try:
+            os.rename(i, path / i.suffix.replace(".", "") / i.name)  # moving files using rename function
+        except:
+            pass
 
 
 def main():
@@ -10,9 +34,12 @@ def main():
 
     path = p.Path(input("Enter Path :"))
     for i in path.glob("*"):
-        print(i)
-        # files.append(i)
-    # print(files)
+        # print(i)
+        files.append(i)
+    get_ext()
+    # print(ext)
+    make_folder()
+    move_files()
 
 
 if __name__ == "__main__":
